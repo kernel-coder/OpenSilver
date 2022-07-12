b2879960e9f9bce087711aa481c12326ce155930
kernel-coder <kernel.writer@gmail.com>
2022-07-12 16:04:00 +0600

    Added SL Controls toolkit

---
+++ src/Runtime/Controls.Toolkit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: ComVisible(false)]
+
+#if __CLSCOMPLIANT__
+[assembly: CLSCompliant(true)]
+#endif
+[assembly: XmlnsPrefix("http://schemas.microsoft.com/winfx/2006/xaml/presentation/toolkit", "toolkit")]
+#if MIGRATION
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation/toolkit", "System.Windows.Controls")]
+#else
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation/toolkit", "Windows.UI.Xaml.Controls")]
+#endif
+
+
+
