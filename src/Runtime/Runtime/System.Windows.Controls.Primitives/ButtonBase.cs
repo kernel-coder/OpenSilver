f3f196593309b6feb8fa555a50fd48f5084e4b9a
Andrey Arekhva <a.arekhva@gmail.com>
2022-09-21 12:11:51 -0400

    Rolling back ButtonBase changes from drag-n-drop feature to fix combobox mouse click

--- src/Runtime/Runtime/System.Windows.Controls.Primitives/ButtonBase.cs
+++ src/Runtime/Runtime/System.Windows.Controls.Primitives/ButtonBase.cs
@@ -260,6 +260,7 @@ namespace Windows.UI.Xaml.Controls.Primitives
 #else
             base.OnPointerPressed(eventArgs);
 #endif
+            eventArgs.Handled = true;
 
 #if MIGRATION
             this.CaptureMouse();
@@ -295,6 +296,7 @@ namespace Windows.UI.Xaml.Controls.Primitives
 #else
                 base.OnPointerReleased(eventArgs);
 #endif
+                eventArgs.Handled = true;
 
                 StopPointerCapture();
 
